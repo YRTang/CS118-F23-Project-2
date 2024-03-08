4908e64d081e4fd57e70b3188bf2d57be9e89340
sarah <sarah@Ubuntu22.04.myguest.virtualbox.org>
2024-03-08 01:37:28 -0800

    new commit

---
+++ client.cpp
@@ -0,0 +1,282 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <math.h>
+#include <queue>
+
+#include "utils.h"
+
+
+void send_packet(struct packet *pkt, int sockfd, struct sockaddr_in *addr, socklen_t addr_size){
+    int bytes_sent = sendto(sockfd, pkt, PACKET_SIZE, 0, (struct sockaddr *)addr, addr_size);
+    if (bytes_sent < 0)
+    {
+        perror("Error sending packet");
+        exit(1);
+    }
+    
+}
+
+// Function that will buffer sent packets for us
+int add_to_buffer(struct packet *pkt, struct packet *buffer, short ack_num)
+{
+    int idx = pkt->seqnum - ack_num;
+    //printf("add packet #%d\n", idx);
+    if (idx < 0)
+    {
+        printf("Already received ACK up to packet %d, which occurs after packet %d\n", ack_num, pkt->seqnum);
+        return -1;
+    }
+    if (idx >= BUFFER_SIZE)
+    {
+        printf("Exceeded maximum window size with packet %d while waiting for ack for %d\n", pkt->seqnum, ack_num);
+        return -1;
+    }
+    buffer[idx] = *pkt;
+    return idx;
+}
+
+// Send all packets within the window (cwnd) starting from first unacked packet
+void send_window_packets(short *seq_num,
+                        short ack_num,
+                        int cwnd,
+                        FILE *fp,
+                        struct packet *pkt, 
+                        int send_sock,
+                        struct sockaddr_in *addr, 
+                        socklen_t addr_size,
+                        struct packet *buffer){
+    int unsent_num = *seq_num - ack_num;
+    int num_to_send = cwnd - unsent_num;
+    printf("\ncwnd=%d, seq_num=%d, ack_num=%d, send %d packets: ", cwnd, *seq_num, ack_num, num_to_send);
+    char payload[PAYLOAD_SIZE];
+    int data_len;
+    printf("\nAdd to buffer: ");
+    for (int i = 0; i < num_to_send; i++)
+    {
+        // Read in the file
+        data_len = fread(payload, 1, PAYLOAD_SIZE, fp);
+        if (data_len < 0)
+        {
+            perror("Error reading file");
+            exit(1);
+        }
+        build_packet(pkt, *seq_num, ack_num, data_len, payload, 0, pkt->total_pck_num);
+        //printf("pkt.length=%d, ", data_len);
+        send_packet(pkt, send_sock, addr, addr_size);
+        add_to_buffer(pkt, buffer, ack_num);
+        printf("buffer[%d]=#%d, ", pkt->seqnum-ack_num, pkt->seqnum);
+        (*seq_num)++;
+    }
+}
+
+void set_socket_timeout(int sockfd, struct timeval timeout)
+{
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0)
+    {
+        perror("Error setting socket timeout");
+        exit(1);
+    }
+}
+
+// This function will only receive ACK from the server
+int recv_ack(int sockfd, struct sockaddr_in *addr, socklen_t addr_size)
+{
+    int ack_num;
+    if (recvfrom(sockfd, &ack_num, sizeof(ack_num), 0, (struct sockaddr *)addr, &addr_size) < 0)
+    {
+        if (errno == ENOTCONN){
+            perror("recv_ack(): connection ends");
+            exit(1);
+        }
+        else if (errno == EWOULDBLOCK || errno == EAGAIN)
+        {
+            // Timeout reached, return -2 to deal with it in the main
+            return -1;
+        }
+        else
+        {
+            perror("recv_ack() failed");
+            exit(1);
+        }
+    }
+    return ack_num;
+}
+
+void update_buffer(struct packet *buffer, int pkts_to_dequeue)
+{
+    printf("delete %d from buffer: ", pkts_to_dequeue);
+    for (int i = pkts_to_dequeue; i < BUFFER_SIZE; i++)
+    {
+        printf("#%d, ", buffer[i-pkts_to_dequeue].seqnum);
+        buffer[i - pkts_to_dequeue] = buffer[i];
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+    int listen_sockfd, send_sockfd;
+    struct sockaddr_in client_addr, server_addr_to, server_addr_from;
+    socklen_t addr_size = sizeof(server_addr_to);
+    struct timeval tv;
+    struct packet pkt;
+    short seq_num = 0;
+    short newly_acked;
+    short ack_num = 0;
+    int cwnd = 0;
+    int ssthresh = 5;
+    int duplicate_ack_count;
+    int timeout_count = 0;
+    struct packet unacked_buffer[BUFFER_SIZE];
+
+    // set timer for packet timeout
+    tv.tv_sec = 0;
+    tv.tv_usec = TIMEOUT * 100000;
+
+    // read filename from command line argument
+    if (argc != 2) {
+        printf("Usage: ./client <filename>\n");
+        return 1;
+    }
+    char *filename = argv[1];
+
+    // Create a UDP socket for listening
+    listen_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (listen_sockfd < 0) {
+        perror("Could not create listen socket");
+        return 1;
+    }
+
+    // Create a UDP socket for sending
+    send_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (send_sockfd < 0) {
+        perror("Could not create send socket");
+        return 1;
+    }
+
+    // Configure the server address structure to which we will send data
+    memset(&server_addr_to, 0, sizeof(server_addr_to));
+    server_addr_to.sin_family = AF_INET;
+    server_addr_to.sin_port = htons(SERVER_PORT_TO);
+    server_addr_to.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    // Configure the client address structure
+    memset(&client_addr, 0, sizeof(client_addr));
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(CLIENT_PORT);
+    client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Bind the listen socket to the client address
+    if (bind(listen_sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0) {
+        perror("Bind failed");
+        close(listen_sockfd);
+        return 1;
+    }
+
+    // Open file for reading
+    FILE *fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        perror("Error opening file");
+        close(listen_sockfd);
+        close(send_sockfd);
+        return 1;
+    }
+
+    // Calculate required packet size
+    fseek(fp, 0, SEEK_END);
+    double file_size = (double)ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    int packet_num = (int)ceil(file_size / (double)PAYLOAD_SIZE);
+    printf("packet_num is %d\n", packet_num);
+
+    // Handshake to establish connection
+    // Do not include data in the packet (empty payload)
+    build_packet(&pkt, seq_num, ack_num, 0, "", 1, packet_num);
+    send_packet(&pkt, send_sockfd, &server_addr_to, addr_size);
+
+    set_socket_timeout(listen_sockfd, tv);
+
+    // the server will send SYN (ack_num) = 1 if the handshake is established
+    int recv_SYN = recv_ack(listen_sockfd, &server_addr_from, addr_size);
+
+    while (recv_SYN != SYN_NUM){
+        send_packet(&pkt, send_sockfd, &server_addr_to, addr_size);
+        recv_SYN = recv_ack(listen_sockfd, &server_addr_from, addr_size);
+    }
+    printf("Connection established\n");
+
+    // TODO: select random seq_num
+    // srand(time(NULL));   // initialization
+    // seq_num = rand();
+
+    // send packets to the server, and receive ACK
+    while (ack_num < packet_num){
+        if (cwnd <= ssthresh){
+            cwnd++;
+        }
+        // controll cwnd size
+        //cwnd = fmin(cwnd, packet_num - seq_num);
+        cwnd = fmin(cwnd, BUFFER_SIZE);
+
+        if (seq_num < packet_num){
+            send_window_packets(&seq_num, ack_num, cwnd, fp, &pkt, send_sockfd, &server_addr_to, addr_size, unacked_buffer);
+        }
+
+        // receive ack
+        newly_acked =  recv_ack(listen_sockfd, &server_addr_from, addr_size);
+        printf("Received ACK=%d\n", newly_acked);
+        
+        // handle ack -> packet lost OR timeout
+        if (newly_acked == -1){
+            // Timeout
+            ssthresh = fmax((int)cwnd / 2, 2);
+            cwnd = INITIAL_WINDOW_SIZE;
+            // resend packet
+            printf("resend packet #%d\n", unacked_buffer[0].seqnum);
+            send_packet(&unacked_buffer[0], send_sockfd, &server_addr_to, addr_size);
+            timeout_count++;
+
+            // Edge cases: Last ACK from the server is lost
+            if (timeout_count > 200){
+                printf("Disconnect\n");
+                break;
+            }
+        }
+        else if (newly_acked == ack_num){
+            duplicate_ack_count++;
+            printf("In loop, duplicate_ack_count=%d\n", duplicate_ack_count);
+            if (duplicate_ack_count == 3){
+                // Fast retransmit
+                ssthresh = fmax((int)cwnd / 2, 2);
+                cwnd += 3;
+                // resend packet
+                printf("resend packet #%d\n", unacked_buffer[0].seqnum);
+                send_packet(&unacked_buffer[0], send_sockfd, &server_addr_to, addr_size);
+            }
+            else if (duplicate_ack_count>3){
+                cwnd++;
+            }
+        }
+        else if (newly_acked < ack_num){
+            // ignore late ack
+            continue;
+        }
+        else{
+            // Normal case
+            update_buffer(unacked_buffer, newly_acked-ack_num);
+            ack_num = newly_acked;
+            duplicate_ack_count = 0;
+            timeout_count = 0;
+        }
+    }
+
+    // Close the socket
+    fclose(fp);
+    close(listen_sockfd);
+    close(send_sockfd);
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,147 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <queue>
+
+#include "utils.h"
+
+using namespace std;
+
+int main() {
+    int listen_sockfd, send_sockfd;
+    struct sockaddr_in server_addr, client_addr_from, client_addr_to;
+    struct packet cur_pkt;
+    socklen_t addr_size = sizeof(client_addr_from);
+    int expected_seq_num = 0;
+    const int syn_num = 1;
+    int buffered_index;
+
+    struct buffer_unit recv_buffer[BUFFER_SIZE];
+    for (int i = 0; i < BUFFER_SIZE; ++i) {
+        recv_buffer[i].is_received = 0;
+    }
+
+    // Create a UDP socket for sending
+    send_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (send_sockfd < 0) {
+        perror("Could not create send socket");
+        return 1;
+    }
+
+    // Create a UDP socket for listening
+    listen_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (listen_sockfd < 0) {
+        perror("Could not create listen socket");
+        return 1;
+    }
+
+    // Configure the server address structure
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Bind the listen socket to the server address
+    if (bind(listen_sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        close(listen_sockfd);
+        return 1;
+    }
+
+    // Configure the client address structure to which we will send ACKs
+    memset(&client_addr_to, 0, sizeof(client_addr_to));
+    client_addr_to.sin_family = AF_INET;
+    client_addr_to.sin_addr.s_addr = inet_addr(LOCAL_HOST);
+    client_addr_to.sin_port = htons(CLIENT_PORT_TO);
+
+    // Open the target file for writing (always write to output.txt)
+    FILE *fp = fopen("output.txt", "wb");
+
+    // handle tcp handshake
+
+    //receive tcp handshake msg and get # packet need to get
+    recvfrom(listen_sockfd, &cur_pkt, PACKET_SIZE, 0, (struct sockaddr *)&client_addr_from, &addr_size);
+    int num_packets = cur_pkt.total_pck_num;
+    printf("Received handshake, num_packets=%d\n", num_packets);
+
+    //send ack back to client
+    sendto(send_sockfd, &syn_num, sizeof(syn_num), 0, (struct sockaddr *)&client_addr_to, addr_size);
+    
+    // Keep receiving potential duplicated hanshake msg
+    // This method can ONLY work when the initial sending window is 1
+    // otherwise, there will be re-order issue of storing data
+    while (1){
+        recvfrom(listen_sockfd, &cur_pkt, PACKET_SIZE, 0, (struct sockaddr *)&client_addr_from, &addr_size);
+        if (cur_pkt.is_handshake){
+            // send ACK back if it is a handshake msg
+            sendto(send_sockfd, &syn_num, sizeof(syn_num), 0, (struct sockaddr *)&client_addr_to, addr_size);
+        }
+        else{
+            // store first non-handshake packet, pkt.seqnum = 0
+            if (cur_pkt.seqnum != 0){
+                // buffer the packet
+                buffered_index = cur_pkt.seqnum - expected_seq_num;
+                recv_buffer[buffered_index].pkt = cur_pkt;
+                recv_buffer[buffered_index].is_received = 1;
+            }
+            else{
+                expected_seq_num=cur_pkt.seqnum+1;
+                fwrite(cur_pkt.payload, 1, cur_pkt.length, fp);
+            }
+            sendto(send_sockfd, &expected_seq_num, sizeof(expected_seq_num), 0, (struct sockaddr *)&client_addr_to, addr_size);
+            printf("receive packet #%d\n", cur_pkt.seqnum);
+            break;
+        }
+    }
+
+
+    // receive all packets
+    while (expected_seq_num < num_packets) {
+        // receive packets
+        recvfrom(listen_sockfd, &cur_pkt, PACKET_SIZE, 0, (struct sockaddr *)&client_addr_from, &addr_size);
+        printf("Receiving packet #%d, ", cur_pkt.seqnum);
+
+        buffered_index = cur_pkt.seqnum - expected_seq_num;
+        if ((buffered_index >= 0) && (buffered_index < BUFFER_SIZE)){
+            // buffer packets
+            if (recv_buffer[buffered_index].is_received == 0){
+                // 0 means not receive this packet before, buffer it
+                recv_buffer[buffered_index].pkt = cur_pkt;
+                recv_buffer[buffered_index].is_received = 1;
+                printf("store at buffer[%d], ", buffered_index);
+            }
+            //write sequenced packets in buffer from head to file
+            int un_written_idx = 0;
+            printf("write to file: ");
+            for (int i = 0; i < BUFFER_SIZE; ++i) {
+                if (!recv_buffer[i].is_received){
+                    un_written_idx = i;
+                    break;
+                }
+                fwrite(recv_buffer[i].pkt.payload, 1, recv_buffer[i].pkt.length, fp);
+                printf("packet #%d, ", recv_buffer[i].pkt.seqnum);
+                expected_seq_num ++;
+            }
+
+            // move un-written file forward
+            for (int i = un_written_idx; i < BUFFER_SIZE; ++i) {
+                recv_buffer[i - un_written_idx] = recv_buffer[i];
+            }
+            for (int i = BUFFER_SIZE - un_written_idx; i < BUFFER_SIZE; ++i) {
+                recv_buffer[i].is_received = 0;
+            }
+        }
+
+        // send ack back to client
+        //int last_recv_seq_num = expected_seq_num - 1;
+        sendto(send_sockfd, &expected_seq_num, sizeof(expected_seq_num), 0, (struct sockaddr *)&client_addr_to, addr_size);
+        printf("send ACK %d\n", expected_seq_num);
+    }
+
+    fclose(fp);
+    close(listen_sockfd);
+    close(send_sockfd);
+    return 0;
+}
